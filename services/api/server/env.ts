bf0b076b08bdddac599ec994663071b1d9f27f22
TrejGun <trejgun@gmail.com>
2020-08-18 10:47:39 +0800

    update node types

--- services/api/server/env.ts
+++ services/api/server/env.ts
@@ -7,7 +7,6 @@ config({
 });
 
 declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace NodeJS {
     // eslint-disable-next-line @typescript-eslint/naming-convention
     interface ProcessEnv {
