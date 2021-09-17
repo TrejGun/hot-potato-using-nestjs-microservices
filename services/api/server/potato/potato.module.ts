d883dff223d4a853e5dc22398add9dd58546c034
TrejGun <trejgun@gmail.com>
2021-09-17 08:55:45 +0800

    feat: open connection in advance

--- services/api/server/potato/potato.module.ts
+++ services/api/server/potato/potato.module.ts
@@ -1,4 +1,6 @@
 import {Module} from "@nestjs/common";
+import {Client, ClientProxy, Transport} from "@nestjs/microservices";
+
 
 import {PotatoController} from "./potato.controller";
 import {PotatoService} from "./potato.service";
@@ -8,4 +10,18 @@ import {PotatoService} from "./potato.service";
   providers: [PotatoService],
   exports: [PotatoService],
 })
-export class PotatoModule {}
+export class PotatoModule {
+  @Client({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RMQ_URL],
+      queue: process.env.RMQ_QUEUE,
+    },
+  })
+  client: ClientProxy;
+
+  public async onApplicationBootstrap(): Promise<void> {
+    console.log("!!!");
+    await this.client.connect();
+  }
+}
